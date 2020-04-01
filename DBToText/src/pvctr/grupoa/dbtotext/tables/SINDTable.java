8a16400036a76cda1bba602104e8e86b37a98103
joaopaulo <joaopaulo@localhost>
2020-04-01 18:26:54 -0300

    Nova atualizcão agora...18:26h

--- DBToText/src/pvctr/grupoa/dbtotext/tables/SINDTable.java
+++ DBToText/src/pvctr/grupoa/dbtotext/tables/SINDTable.java
@@ -31,10 +31,27 @@ public class SINDTable extends GenericTable {
 				dbl.getFields().add(new StringField(10, "01/01/1900"));
 				dbl.getFields().add(new StringField(20, "0"));
 				dbl.getFields().add(new StringField(2, "1"));
-				dbl.getFields().add(new NumberField(18, Utilities.treatCNPJ(rs.getString("CNPJ"))));
-				dbl.getFields().add(new StringField(2, rs.getString("UFD_SIGLA_CTPS")));
-				dbl.getFields().add(new DateField(10, rs.getTimestamp("CTPSDTEXPEDICAO")));				
-				dbl.getFields().add(new StringField(3, "000"));
+				dbl.getFields().add(new StringField(18, Utilities.treatCNPJ(rs.getString("CNPJ"))));
+				dbl.getFields().add(new StringField(5, ""));
+				dbl.getFields().add(new StringField(6, ""));
+				dbl.getFields().add(new StringField(21, "33"));
+				dbl.getFields().add(new StringField(40, rs.getString("ENDLOGRADOURO")));
+				dbl.getFields().add(new NumberField(7, rs.getInt("ENDNUMERO")));
+				dbl.getFields().add(new StringField(20, rs.getString("ENDCOMPLEMENTO")));
+				dbl.getFields().add(new StringField(30, rs.getString("ENDBAIRRO")));
+				dbl.getFields().add(new StringField(2, rs.getString("MUN_UFD_SIGLA")));
+				dbl.getFields().add(new NumberField(8, Utilities.getCodmunicipio(rs.getString("MUN_UFD_SIGLA"), rs.getString("MUN_CODIGO")))); //codigomunic - verificar alteração deste campo
+				dbl.getFields().add(new StringField(10, Utilities.treatCEP(rs.getString("CEP")))); //cep
+				dbl.getFields().add(new StringField(7, ""));
+				dbl.getFields().add(new StringField(5, rs.getString("DDD")));
+				dbl.getFields().add(new StringField(10, rs.getString("FONE")));
+				dbl.getFields().add(new StringField(5, ""));
+				dbl.getFields().add(new StringField(10, ""));
+				dbl.getFields().add(new StringField(100, rs.getString("EMAIL")));
+				dbl.getFields().add(new StringField(100, ""));
+				dbl.getFields().add(new StringField(1, "0"));
+				dbl.getFields().add(new StringField(5, ""));
+				dbl.getFields().add(new StringField(3, "0"));
 					
 				//ALTERACAO E DAQUI PRA CIMA
 				this.lines.add(dbl);
